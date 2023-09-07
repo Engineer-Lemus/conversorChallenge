4143273d4a9b37d81a1bb6624192fd86df865490
Dany Lemus <software.engineer.lemus@gmail.com>
2023-09-06 19:54:30 -0600

    Anadiendo primera prueba de conversor de monedas

---
+++ ConversorMonedasAlura/src/Conversor/Moneda.java
@@ -0,0 +1,49 @@
+package conversor;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.Properties;
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLEditorKit.Parser;
+
+import org.json.JSONObject;
+
+public class Moneda {
+
+	private String nombre;
+	private double cantidad;
+
+	public Moneda(String nombre, double cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+	}
+
+	public Moneda(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+}
